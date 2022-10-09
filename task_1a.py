c7423c217b61c2ceef677eccec8d0d1cdd22739e
Saran-codes <101175195+Saran-codes@users.noreply.github.com>
2022-10-09 14:29:31 +0530

    Add files via upload

--- task_1a.py
+++ task_1a.py
@@ -105,7 +105,7 @@ def which_colour(img,points):
         if (img[points[1]][points[0]] == bgr[i]).all():
             return colours[i]
         
-def shape_detector(img_grid,node):
+def shape_detector(img_grid,node,maze_img):
     
     if len(img_grid.shape) == 3 :
         gray = cv2.cvtColor(img_grid,cv2.COLOR_BGR2GRAY)
@@ -128,15 +128,15 @@ def shape_detector(img_grid,node):
                 if aspectratio >=0.95 and aspectratio <= 1.05:
                     centres.append(centroid(approx,node))
                     shapes.append("Square")
-                    colours.append(which_colour(maze_image,centroid(approx,node)))
+                    colours.append(which_colour(maze_img,centroid(approx,node)))
             elif nofcorners == 3 and cv2.contourArea(i)>240:
                 centres.append(centroid(approx,node))
                 shapes.append("Triange")
-                colours.append(which_colour(maze_image,centroid(approx,node)))
+                colours.append(which_colour(maze_img,centroid(approx,node)))
             elif cv2.contourArea(i)>380:
                 centres.append(centroid(approx,node))
                 shapes.append("Circle")
-                colours.append(which_colour(maze_image,centroid(approx,node)))
+                colours.append(which_colour(maze_img,centroid(approx,node)))
     return shapes[::-1],centres[::-1],colours[::-1]
 
 address = ["A1","A2","A3","A4","A5","A6","A7","B1","B2","B3","B4","B5","B6","B7","C1","C2","C3","C4","C5","C6","C7","D1","D2","D3","D4","D5","D6","D7","E1","E2","E3","E4","E5","E6","E7","F1","F2","F3","F4","F5","F6","F7","G1","G2","G3","G4","G5","G6","G7"]
@@ -176,7 +176,7 @@ def detect_medicine_packages(img):
         if i%6==0:
             node = grids(first_node)[i]
             img_grid = warp(img,node)[0]
-            shapes,centres,colours = shape_detector(img_grid,node)
+            shapes,centres,colours = shape_detector(img_grid,node,img)
             for j in range(len(shapes)):
                 tmp = []
                 tmp.append(shops[i//6])
